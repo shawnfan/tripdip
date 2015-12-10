229ec482d67badf39918fc8704df918a08f19568
shawnfan <zhangfan.shawn@gmail.com>
2015-12-10 00:14:11 -0500

    added yelp pic

--- app/assets/javascripts/trip/tripCtrl.js
+++ app/assets/javascripts/trip/tripCtrl.js
@@ -3,6 +3,12 @@ angular.module('angularApp')
 .controller('TripCtrl', [
 '$scope',
 function($scope){
-	$scope.photo = "https://s3.amazonaws.com/tripdip/travel/_AK49850.JPG";
+	$scope.photo = "https://s3.amazonaws.com/tripdip/DinnerWithFriend.jpg";
 	$scope.mapPic = "https://s3.amazonaws.com/tripdip/GoogleMap.png";
+	$scope.yelpPic = "https://s3.amazonaws.com/tripdip/yelppic.jpg";
+
+	$scope.clicked = false;
+	$scope.click = function() {
+		$scope.clicked = true;
+	};
 }]);
