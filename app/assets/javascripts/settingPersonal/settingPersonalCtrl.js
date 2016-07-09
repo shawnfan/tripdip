360e8a1f25ef7d8a425bfc6dcc3bee01d8426b0a
shawnfan <zhangfan.shawn@gmail.com>
2016-07-09 00:59:49 -0700

    added personality page

--- app/assets/javascripts/settingPersonal/settingPersonalCtrl.js
+++ app/assets/javascripts/settingPersonal/settingPersonalCtrl.js
@@ -1,17 +1,59 @@
 "use strict"
 angular.module('angularApp')
 .controller('SettingPersonalCtrl', [
-'$scope',
-function($scope){
-	$scope.personImg = [
-		"https://s3.amazonaws.com/tripdip/v2/p_animal.jpg",
-		"https://s3.amazonaws.com/tripdip/v2/p_arty.jpg",
-		"https://s3.amazonaws.com/tripdip/v2/p_clubbing.jpg",
-		"https://s3.amazonaws.com/tripdip/v2/p_eatOut.jpg",
-		"https://s3.amazonaws.com/tripdip/v2/p_museum.jpg",
-		"https://s3.amazonaws.com/tripdip/v2/p_outdoor.jpg",
-		"https://s3.amazonaws.com/tripdip/v2/p_relaxing.jpg",
-		"https://s3.amazonaws.com/tripdip/v2/p_social.jpg"
+'$scope','$mdDialog',
+function($scope, $mdDialog){
+	$scope.personalityImg = [
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_animals.jpg",
+			"title": "Animals"
+		},
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_arty.jpg",
+			"title": "Arty"
+		},
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_clubbing.jpg",
+			"title": "Clubbing"
+		},
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_eatOut.jpg",
+			"title": "Eat Out"
+		},
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_museum.jpg",
+			"title": "Museum"
+		},
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_outdoor.jpg",
+			"title": "Outdoor"
+		},
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_relaxing.jpg",
+			"title": "Realaxing"
+		},
+		{
+			"img": "https://s3.amazonaws.com/tripdip/v2/p_social.jpg",
+			"title": "Social"
+		}	
 	];
+
+	$scope.confirm = function() {
+		alert("You have successfully submitted your personality quiz to your profile!");
+	};
+
+	$scope.showConfirm = function(ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    var confirm = $mdDialog.confirm()
+          .title('You have successfully submitted your personality quiz to your profile!')
+          .targetEvent(ev)
+          .ok('OK')
+    $mdDialog.show(confirm).then(function() {
+      $scope.status = 'ok';
+    }, function() {
+      $scope.status = 'cancel';
+    });
+  };
+
 
 }]);
