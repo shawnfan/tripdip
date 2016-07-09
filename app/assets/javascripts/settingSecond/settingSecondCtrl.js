8efb2dfc8d69f0d1ab816b2235e2a363a9887728
shawnfan <zhangfan.shawn@gmail.com>
2016-07-08 22:52:31 -0700

    Add setting page 2

--- app/assets/javascripts/settingSecond/settingSecondCtrl.js
+++ app/assets/javascripts/settingSecond/settingSecondCtrl.js
@@ -3,5 +3,15 @@ angular.module('angularApp')
 .controller('SettingSecondCtrl', [
 '$scope',
 function($scope){
+	$scope.map = "https://s3.amazonaws.com/tripdip/GoogleMap.png";
+	$scope.mainColor = "#F38181";
 
+	$scope.result = [
+		{"day": "Monday (8/1)",		"time": "8:45am-9:45am",		"seq": 1, 	"loc":"Four Seasons Hotel"},
+		{"day": "Monday (8/1)",		"time": "10:45am-11:45am",		"seq": 2, 	"loc":"Sam's Café (Breakfast)"},
+		{"day": "Tuesday (8/2)",	"time": "12:30pm-2:30pm",		"seq": 3, 	"loc":"Hangzhou Zoo"},
+		{"day": "Wednesday (8/3)",	"time": "3:00pm-5:00pm",		"seq": 4, 	"loc":"Zhejiang Tea Museum"},
+		{"day": "Thursday (8/4)",	"time": "5:45pm-7:45pm",		"seq": 5, 	"loc":"Shan Wai Shan Restaurant (Dinner)"},
+		{"day": "Friday (8/5)",		"time": "9:30pm-10:30pm",		"seq": 6, 	"loc":"Black Swan Bar"}
+	];
 }]);
