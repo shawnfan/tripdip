ff6b410800f6d43153cc00dcf103e995f01aca34
shawnfan <zhangfan.shawn@gmail.com>
2016-07-09 02:11:35 -0700

    update layout

--- app/assets/javascripts/settingSecond/settingSecondCtrl.js
+++ app/assets/javascripts/settingSecond/settingSecondCtrl.js
@@ -5,13 +5,19 @@ angular.module('angularApp')
 function($scope){
 	$scope.map = "https://s3.amazonaws.com/tripdip/GoogleMap.png";
 	$scope.mainColor = "#F38181";
-
+	$scope.days = [
+		"  Monday (8/1)    ",
+		"  Tuesday (8/2)   ",
+		"  Wednesday (8/3) ",
+		"  Thursday (8/4)  ",
+		"  Friday (8/5)    "
+	];
 	$scope.result = [
-		{"day": "Monday (8/1)",		"time": "8:45am-9:45am",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_hotel.png", 	"loc":"Four Seasons Hotel"},
-		{"day": "Monday (8/1)",		"time": "10:45am-11:45am",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_restaurant.png", 	"loc":"Sam's Café (Breakfast)"},
-		{"day": "Tuesday (8/2)",	"time": "12:30pm-2:30pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_zoo.png", 	"loc":"Hangzhou Zoo"},
-		{"day": "Wednesday (8/3)",	"time": "3:00pm-5:00pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_museum.png", 	"loc":"Zhejiang Tea Museum"},
-		{"day": "Thursday (8/4)",	"time": "5:45pm-7:45pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_restaurant.png", 	"loc":"Shan Wai Shan Restaurant (Dinner)"},
-		{"day": "Friday (8/5)",		"time": "9:30pm-10:30pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_bar.png", 	"loc":"Black Swan Bar"}
+		{"time": "8:45am-9:45am",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_hotel.png", 	"loc":"Four Seasons Hotel"},
+		{"time": "10:45am-11:45am",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_restaurant.png", 	"loc":"Sam's Café (Breakfast)"},
+		{"time": "12:30pm-2:30pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_zoo.png", 	"loc":"Hangzhou Zoo"},
+		{"time": "3:00pm-5:00pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_museum.png", 	"loc":"Zhejiang Tea Museum"},
+		{"time": "5:45pm-7:45pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_restaurant.png", 	"loc":"Shan Wai Shan Restaurant (Dinner)"},
+		{"time": "9:30pm-10:30pm",		"icon": "https://s3.amazonaws.com/tripdip/v2/icon_bar.png", 	"loc":"Black Swan Bar"}
 	];
 }]);
