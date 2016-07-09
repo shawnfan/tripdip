4cdf7cddfa9b377c8302b35a48437f5f4b3794c9
shawnfan <zhangfan.shawn@gmail.com>
2016-07-08 23:58:49 -0700

    large text

--- app/assets/javascripts/settingPersonal/settingPersonalCtrl.js
+++ app/assets/javascripts/settingPersonal/settingPersonalCtrl.js
@@ -3,5 +3,15 @@ angular.module('angularApp')
 .controller('SettingPersonalCtrl', [
 '$scope',
 function($scope){
+	$scope.personImg = [
+		"https://s3.amazonaws.com/tripdip/v2/p_animal.jpg",
+		"https://s3.amazonaws.com/tripdip/v2/p_arty.jpg",
+		"https://s3.amazonaws.com/tripdip/v2/p_clubbing.jpg",
+		"https://s3.amazonaws.com/tripdip/v2/p_eatOut.jpg",
+		"https://s3.amazonaws.com/tripdip/v2/p_museum.jpg",
+		"https://s3.amazonaws.com/tripdip/v2/p_outdoor.jpg",
+		"https://s3.amazonaws.com/tripdip/v2/p_relaxing.jpg",
+		"https://s3.amazonaws.com/tripdip/v2/p_social.jpg"
+	];
 
 }]);
